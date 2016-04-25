8f7b8d7f00f4428fe8faf6dcf5bb68e5c8d11883
ashley <kilgourkilgour@hotmail.com>
2016-04-25 14:56:32 +0100

    Added logic to get template details

--- SignableSendAPIDemo/Program.cs
+++ SignableSendAPIDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace SignableSendAPIDemo
 {
     class Program
     {
+        static string getTemplates = "https://api.signable.co.uk/v1/templates?offset=0&limit=3";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Start");
@@ -15,11 +18,30 @@ namespace SignableSendAPIDemo
             Console.WriteLine("Enter your API Key from  https://app.signable.co.uk/api");
             string apiKey = Console.ReadLine();
 
-            var bytes = Encoding.UTF8.GetBytes(apiKey + ":x");
-            var base64 = Convert.ToBase64String(bytes);
+            var webClient = CreateWebClient(apiKey);
+
+            GetDetail(webClient, getTemplates);
 
             Console.WriteLine("End");
             Console.WriteLine("Press any key to end ...");
+            Console.ReadLine();
+        }
+
+        private static WebClient CreateWebClient(string apiKey)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiKey + ":x"));
+            var authorization = string.Format("Basic {0}", credentials);
+            webClient.Headers[HttpRequestHeader.Authorization] = authorization;
+            return webClient;
+        }
+
+        private static void GetDetail(WebClient webClient, string apiCommand)
+        {
+            var results = webClient.DownloadString(apiCommand);
+            Console.WriteLine(results);
+            Console.WriteLine("Press any key to continue ...");
             Console.ReadLine();
         }
     }

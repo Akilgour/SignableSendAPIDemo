e7369e68a20eb4fc8545025ece32254daacf1c6f
ashley <kilgourkilgour@hotmail.com>
2016-04-25 12:26:04 +0100

    Enter API password

--- SignableSendAPIDemo/Program.cs
+++ SignableSendAPIDemo/Program.cs
@@ -12,6 +12,12 @@ namespace SignableSendAPIDemo
         {
             Console.WriteLine("Start");
 
+            Console.WriteLine("Enter your API Key from  https://app.signable.co.uk/api");
+            string apiKey = Console.ReadLine();
+
+            var bytes = Encoding.UTF8.GetBytes(apiKey + ":x");
+            var base64 = Convert.ToBase64String(bytes);
+
             Console.WriteLine("End");
             Console.WriteLine("Press any key to end ...");
             Console.ReadLine();
